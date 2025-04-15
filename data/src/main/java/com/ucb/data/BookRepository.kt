81fa526982cb5558c70650b453979ced91dd54c5
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2025-04-15 16:58:14 -0400

    Creacion de BookRepository en data

---
+++ data/src/main/java/com/ucb/data/BookRepository.kt
@@ -0,0 +1,17 @@
+package com.ucb.data
+
+import com.ucb.data.book.IBookLocalDataSource
+import com.ucb.data.book.IBookRemoteDataSource
+import com.ucb.domain.Book
+
+class BookRepository(
+    private val remote: IBookRemoteDataSource,
+    private val local: IBookLocalDataSource) {
+    suspend fun findBook(titulo: String): NetworkResult<Book> {
+        return this.remote.buscar(titulo)
+    }
+
+    suspend fun likeBook(book: Book): Boolean {
+        return this.local.likeBook(book)
+    }
+}
