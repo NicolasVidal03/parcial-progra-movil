365ba1605ed27d342544216442ccb5f12728a318
NicolasVidal03 <nicolas.vidal@ucb.edu.bo>
2025-04-15 17:02:34 -0400

    Arreglo en el repository

--- data/src/main/java/com/ucb/data/BookRepository.kt
+++ data/src/main/java/com/ucb/data/BookRepository.kt
@@ -7,11 +7,15 @@ import com.ucb.domain.Book
 class BookRepository(
     private val remote: IBookRemoteDataSource,
     private val local: IBookLocalDataSource) {
-    suspend fun findBook(titulo: String): NetworkResult<Book> {
+    suspend fun findBook(titulo: String): NetworkResult<List<Book>> {
         return this.remote.buscar(titulo)
     }
 
     suspend fun likeBook(book: Book): Boolean {
         return this.local.likeBook(book)
     }
-}
+
+    suspend fun getLikedBooks(): List<Book> {
+        return this.local.getLikedBooks()
+    }
+}g
--- data/src/main/java/com/ucb/data/book/IBookLocalDataSource.kt
+++ data/src/main/java/com/ucb/data/book/IBookLocalDataSource.kt
@@ -1,9 +1,8 @@
 package com.ucb.data.book
 
 import com.ucb.domain.Book
-import com.ucb.domain.Movie
 
 interface IBookLocalDataSource {
     suspend fun likeBook(book: Book): Boolean
-    suspend fun getLikedBooks(): List<Movie>
+    suspend fun getLikedBooks(): List<Book>
 }
